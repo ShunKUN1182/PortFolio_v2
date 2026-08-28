f825a38b8ae603a8fd49623aba4248fd1b37e249
ShunKUN1182 <2250378@ecc.ac.jp>
2026-08-28 15:44:37 +0900

    feat: ressの導入,Layoutの完成

---
+++ src/scripts/const.ts
@@ -0,0 +1,5 @@
+export const lang: string = "ja";
+export const title: string = "フクシマ・ポートフォリオ";
+export const description: string = "福島駿弥のポートフォリオです";
+export const x_name: string = "fksm_0614";
+export const url: string = "https://comp.ecc.ac.jp/";
